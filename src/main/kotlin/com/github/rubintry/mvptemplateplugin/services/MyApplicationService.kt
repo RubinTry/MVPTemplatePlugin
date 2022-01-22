f0ec407d90fedc769ac0c0bc13df0a725b323d27
GitHub Action <action@github.com>
2022-01-22 08:22:36 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/rubintry/mvptemplateplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.rubintry.mvptemplateplugin.services
+
+import com.github.rubintry.mvptemplateplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
